899ad7a9b4916dfee4574872ac0937100cdee694
Ferrokwastaken <israelduquesilva@hotmail.com>
2024-09-23 17:16:19 +0100

    Arrays test

---
+++ EjerciciosArraysPasoEntrePáginas/procesado_formulario_provincias.php
@@ -0,0 +1,15 @@
+<?php
+    require_once('datos_provincias.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP de provincias</title>
+</head>
+<body>
+    
+</body>
+</html>
